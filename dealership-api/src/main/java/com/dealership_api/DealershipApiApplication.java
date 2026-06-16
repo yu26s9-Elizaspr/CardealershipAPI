eaa1906a8afafc7972321ef45a85e01d1641a564
Elizaspr <spratt.eliza@gmail.com>
2026-06-16 13:54:10 -0400

    trying to add spring

---
+++ dealership-api/src/main/java/com/dealership_api/DealershipApiApplication.java
@@ -0,0 +1,13 @@
+package com.dealership_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipApiApplication.class, args);
+	}
+
+}
---
+++ dealership-api/src/test/java/com/dealership_api/DealershipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealership_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
