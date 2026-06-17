0e30b2651efb2ffae015370626b67f31a69a1ae9
Elizaspr <spratt.eliza@gmail.com>
2026-06-17 15:26:04 -0400

    fixed prop and vehicle syntax

--- dealership-api/src/main/java/com/dealership_api/Models/Vehicle.java
+++ dealership-api/src/main/java/com/dealership_api/Models/Vehicle.java
@@ -1,13 +1,16 @@
 package com.dealership_api.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Vehicle {
 
     @Id
-   private int vin;
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+   private Integer vin;
 
    private int year;
    private String make;
